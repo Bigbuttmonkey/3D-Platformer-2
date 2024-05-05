ad780958c6bd481ba914b449f28da5e8a436a62c
Bigbuttmonkey <168965184+Bigbuttmonkey@users.noreply.github.com>
2024-05-05 17:30:14 +0700

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    int a = 10;
+    float b = 40.3f;
+    double c = 40;
+
+    string text = "ahyfbyiue";
+    bool isRunning = false;
+    int helloThisIsAText = 69;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("d ytdsgiyerwvgubobweiufgweihfowei0fhwv");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(1);
+    }
+}
